afa1cc2d756aa6710a0677319725a1fc565d2697
eshanikarunarathne <karunarathneasinsala@gmail.com>
2018-02-02 15:08:20 +0530

    first eshani

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //database: 'mongodb://crowd:crowdNawala@ds115758.mlab.com:15758/crowddb',
+  database: 'mongodb://localhost:27017/crowdDB',
+  secret: '43nkjhiouh@*&(9384h3(45fs644sfjefnmneji2ihdna1*&@!&#%^&GHBH!Y#*&!IHJi!U@yy3ui1YYH5143651x4',
+}
