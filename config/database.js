d0bfb9301253320899d4b6a0f351b33fdb4aa28c
eshanikarunarathne <karunarathneasinsala@gmail.com>
2018-02-02 15:29:15 +0530

    second esh

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
   //database: 'mongodb://crowd:crowdNawala@ds115758.mlab.com:15758/crowddb',
-  database: 'mongodb://localhost:27017/crowdDB',
+  database: 'mongodb://crowd:level2crowd@ds121248.mlab.com:21248/crowddb',
+ // database: 'mongodb://localhost:27017/crowdDB',
   secret: '43nkjhiouh@*&(9384h3(45fs644sfjefnmneji2ihdna1*&@!&#%^&GHBH!Y#*&!IHJi!U@yy3ui1YYH5143651x4',
 }
